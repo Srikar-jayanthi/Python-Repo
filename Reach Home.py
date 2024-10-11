9f3d26d1da7c3867b71623944392a139c2a4652b
Jayanthi Srikar <147791947+Srikar-jayanthi@users.noreply.github.com>
2024-10-11 22:18:51 +0530

    Uploading code: Reach Home.py

---
+++ Reach Home.py
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Rh{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int x,y;
+        x=sc.nextInt();
+        y=sc.nextInt();
+        if(x*5>=y){
+            System.out.println("YES");
+
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}
